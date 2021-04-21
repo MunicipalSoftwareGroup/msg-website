f355c873f6e2d2bd505a9719f158da120c2de570
cbrit <collinbrittain@gmail.com>
2021-04-21 09:42:06 -0500

    Scroll menu behind splash

--- js/highlightScrollMenu.js
+++ js/highlightScrollMenu.js
@@ -5,21 +5,24 @@ function highlight() {
     var ourSoftware = document.getElementsByName("pitch-section")[0];
     var aboutUs = document.getElementsByName("about-us-section")[0];
     var contactUs = document.getElementsByName("contact-section")[0];
-    var threshold = 100;
-    if (pos >= ourSoftware.offsetTop - threshold && pos < aboutUs.offsetTop - threshold) {
-        console.log("Our Software");
+    var threshold = 200;
+    
+    if (pos < ourSoftware.offsetTop - threshold) {
+        items[1].style.color = "#707070";
+        items[2].style.color = "#707070";
+        items[3].style.color = "#707070";
+    }
+    else if (pos >= ourSoftware.offsetTop - threshold && pos < aboutUs.offsetTop - threshold) {
         items[1].style.color = "#FAC173";
         items[2].style.color = "#707070";
         items[3].style.color = "#707070";
     }
     else if (pos >= aboutUs.offsetTop - threshold && pos < contactUs.offsetTop - threshold) {
-        console.log("About Us");
         items[1].style.color = "#707070";
         items[2].style.color = "#FAC173";
         items[3].style.color = "#707070";
     }
     else if (pos >= contactUs.offsetTop - threshold) {
-        console.log(items[3]);
         items[1].style.color = "#707070";
         items[2].style.color = "#707070";
         items[3].style.color = "#FAC173";
