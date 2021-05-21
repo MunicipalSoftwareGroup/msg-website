8b29120c23eb26c4630c34b78e2c591335b85c30
Collin Brittain <municipalsoftware@outlook.com>
2021-05-21 16:25:30 -0500

    Stuff I might have to fix later because forgot what it is...

--- js/demo.js
+++ js/demo.js
@@ -1,84 +1,95 @@
 function startDemo() {
-    var slide = document.getElementById("slide");
-    
-    slide.src = "images\\slides\\0.png";
-    setCurrentSlide(0);
-    showDemo();
+	var slide = document.getElementById("slide");
+
+	slide.src = "images\\slides\\0.png";
+	setCurrentSlide(0);
+	showDemo();
 }
 
 function showDemo() {
-    var demo = document.getElementById("demo-container");
-    
-    if (demo.style.display != "flex")
-        demo.style.display = "flex";
+	var demo = document.getElementById("demo-container");
+
+	if (demo.style.display != "flex") {
+		demo.style.display = "flex";
+	}
+}
+
+function checkClose(event) {
+	var inner = document.getElementById("demo-container-inner");
+
+	if (!inner.contains(event.target)) {
+		hideDemo();
+	}
 }
 
 function hideDemo() {
-    var demo = document.getElementById("demo-container");
-    
-    if (demo.style.display != "none")
-        demo.style.display = "none";
+	var demo = document.getElementById("demo-container");
+
+	if (demo.style.display != "none") {
+		demo.style.display = "none";
+	}
 }
 
 async function nextSlide() {
-    var slide = document.getElementById("slide");
-    var slides = await getSlides();
-    slides.keys
-    var num = getCurrentSlide();
-    var newNum = num + 1;
-    
-    if (slides[newNum + 1] === undefined) {
-        console.error("Slide is null");
-        return;
-    }
-    
-    slide.src = getSlidesPath() + slides[newNum];
-    setCurrentSlide(newNum);
+	var slide = document.getElementById("slide");
+	var slides = await getSlides();
+	slides.keys;
+	var num = getCurrentSlide();
+	var newNum = num + 1;
+
+	if (slides[newNum + 1] === undefined) {
+		console.error("Slide is null");
+		return;
+	}
+
+	slide.src = getSlidesPath() + slides[newNum];
+	setCurrentSlide(newNum);
 }
 
 async function prevSlide() {
-    var slide = document.getElementById("slide");
-    var slides = await getSlides();
-    var num = getCurrentSlide();
-    var newNum = num - 1;
-    
-    if (newNum < 0)
-        return;
-    
-    if (slides[newNum] === undefined) {
-        console.error("Slide is null");
-        return;
-    }
-    
-    slide.src = getSlidesPath() + slides[newNum];
-    setCurrentSlide(newNum);
+	var slide = document.getElementById("slide");
+	var slides = await getSlides();
+	var num = getCurrentSlide();
+	var newNum = num - 1;
+
+	if (newNum < 0) return;
+
+	if (slides[newNum] === undefined) {
+		console.error("Slide is null");
+		return;
+	}
+
+	slide.src = getSlidesPath() + slides[newNum];
+	setCurrentSlide(newNum);
 }
 
 async function openSlide(newNum) {
-    var slide = document.getElementById("slide");
-    var slides = await getSlides();
-    var num = getCurrentSlide();
-    
-    slide.src = getSlidesPath() + slides[newNum];
-    setCurrentSlide(newNum);
-    showDemo();
+	var slide = document.getElementById("slide");
+	var slides = await getSlides();
+	var num = getCurrentSlide();
+
+	slide.src = getSlidesPath() + slides[newNum];
+	setCurrentSlide(newNum);
+	showDemo();
 }
 
 function getCurrentSlide() {
-    var p = document.getElementById("current-slide");
-    
-    return parseInt(p.innerHTML, 10);
+	var p = document.getElementById("current-slide");
+
+	return parseInt(p.innerHTML, 10);
 }
 
 function setCurrentSlide(num) {
-    var p = document.getElementById("current-slide");
-    p.innerHTML = num;
+	var p = document.getElementById("current-slide");
+	p.innerHTML = num;
 }
 
 function getSlidesPath() {
-    return "images\\slides\\";
+	return "images\\slides\\";
 }
 
 async function getSlides() {
-    return await $.getJSON("demo.json", null).then(function (json) { return json.slides });
+	return await $.getJSON("demo.json", null).then(function (json) {
+		return json.slides;
+	});
 }
