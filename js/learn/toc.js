38365b801709f7046873d53e9fdfd93fea4f15a9
Collin Brittain <municipalsoftware@outlook.com>
2021-07-05 10:25:41 -0500

    Comment out old dropdown code

--- js/learn/toc.js
+++ js/learn/toc.js
@@ -1,15 +1,15 @@
-$(document).on('click', '.dropdown', function() {
+// $(document).on('click', '.dropdown', function() {
 
-        if (!$(this).children('.chevron').hasClass('rotate90chevron')) {
-            console.log("rotate1");
-            $(this).children('.chevron').addClass('rotate90chevron');
-        } else {
-            console.log("rotate2");
-            $(this).children('.chevron').removeClass('rotate90chevron');
-        }
+//         if (!$(this).children('.chevron').hasClass('rotate90chevron')) {
+//             console.log("rotate1");
+//             $(this).children('.chevron').addClass('rotate90chevron');
+//         } else {
+//             console.log("rotate2");
+//             $(this).children('.chevron').removeClass('rotate90chevron');
+//         }
 
-        $(this).parent().children('ul').toggle();
-    });
+//         $(this).parent().children('ul').toggle();
+//     });
 
 function dropdown(id) {
     var dropdown = document.getElementById(id);
